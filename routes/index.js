9698552c75686b59a8f6d1a03997f0fb041c5418
sakamotorafael <rafael.sakamoto@gmail.com>
2021-03-08 18:39:31 -0300

    corrigi conflitos e melhorei as rotas para a area logada

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/indexController');
 const testController = require('../controllers/testController');
-const loggedController = require('../controllers/loggedController')
 const notLogged = require('../middlewares/notLogged')
-const isLogged = require('../middlewares/isLogged')
 
-
-// Se usuário não-logado, indexController
 router.get('/', notLogged, indexController.index)
 router.get('/login', notLogged, indexController.login)
 router.get('/cadastro', notLogged, indexController.cadastro)
