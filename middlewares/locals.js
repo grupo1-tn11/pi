25e89b3e9cc0c0b669ec6a9603befbae9ffdf984
sakamotorafael <rafael.sakamoto@gmail.com>
2021-03-16 16:00:41 -0300

    resolvido errinho no index

--- middlewares/locals.js
+++ middlewares/locals.js
@@ -4,6 +4,8 @@ module.exports = (req, res, next) => {
   const usuario = req.session.usuario
   if (usuario) {
     res.locals.usuario = usuario
+  } else {
+    res.locals.usuario = null
   }
   next()
 }
