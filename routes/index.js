a0cfc5b5450986660e470b6affab8c357b820a69
sakamotorafael <rafael.sakamoto@gmail.com>
2021-03-16 15:36:01 -0300

    ajuste no middleware de autenticacao

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
-const verificaAutenticacao = require('../middlewares/auth')
+
 const locals = require('../middlewares/locals')
 const autenticar = require('../middlewares/autenticar')
 
