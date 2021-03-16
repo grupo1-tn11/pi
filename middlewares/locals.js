ba94ddca16041cc849b0dfed46e6950ffef9dd3f
sakamotorafael <rafael.sakamoto@gmail.com>
2021-03-16 15:33:58 -0300

    ajuste no middleware de autenticacao

--- middlewares/locals.js
+++ middlewares/locals.js
@@ -4,6 +4,5 @@
 module.exports = (req, res, next) => {
   const usuario = req.session.usuario
   res.locals.usuario = usuario
-  
   next()
 }
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
 const verificaAutenticacao = require('../middlewares/auth')
-const verificaLocals = require('../middlewares/locals')
+const locals = require('../middlewares/locals')
 const autenticar = require('../middlewares/autenticar')
 
 /* GET home page. */
-router.get("/", autenticar.seLogadoNext, indexController.index);
+router.get("/", locals, indexController.index);
 router.get('/logout', autenticar.seLogadoNext, indexController.logout)
 
 module.exports = router;
