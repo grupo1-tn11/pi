cf9c0e86b6a30bb5233b143ebeea433dfdbc7f6a
luucaspaixao <luucaspaixao1997@yahoo.com>
2021-04-11 14:57:39 -0300

    Ajuste para quando não houver informações no body referente as redes sociais, competencias, etc

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -56,6 +56,8 @@ const controller = {
 
     //console.log('body - ', body)
 
+    console.log(body)
+
     try {
       if (file) {
         const updatedUsuario = await Usuarios.update(
@@ -83,86 +85,95 @@ const controller = {
       console.log('usuario - ', error)
     }
 
-    try {
-      await Competencias.destroy({
-        where: { usuarios_id: id },
-      })
-      for (let i = 0; i < body.competencias.length; i++) {
-        await Competencias.create({
-          nome: body.competencias[i],
-          usuarios_id: id,
+    if(body.competencias){
+      try {
+        await Competencias.destroy({
+          where: { usuarios_id: id },
         })
+        for (let i = 0; i < body.competencias.length; i++) {
+          await Competencias.create({
+            nome: body.competencias[i],
+            usuarios_id: id,
+          })
+        }
+      } catch (error) {
+        console.log('competencias - ', error)
       }
-    } catch (error) {
-      console.log('competencias - ', error)
     }
 
-    try {
-      await Experiencia_pro.destroy({
-        where: { usuarios_id: id },
-      })
-      for (let i = 0; i < body.expCargo.length; i++) {
-        await Experiencia_pro.create({
-          empresa: body.expEmpresa[i],
-          cargo: body.expCargo[i],
-          funcao: body.expFuncao[i],
-          descricao: body.expDescricao[i],
-          inicio: body.expInicio[i],
-          termino: body.expTermino[i],
-          usuarios_id: id,
+    if(body.expCargo){
+      try {
+        await Experiencia_pro.destroy({
+          where: { usuarios_id: id },
         })
+        for (let i = 0; i < body.expCargo.length; i++) {
+          await Experiencia_pro.create({
+            empresa: body.expEmpresa[i],
+            cargo: body.expCargo[i],
+            funcao: body.expFuncao[i],
+            descricao: body.expDescricao[i],
+            inicio: body.expInicio[i],
+            termino: body.expTermino[i],
+            usuarios_id: id,
+          })
+        }
+      } catch (error) {
+        console.log('experiencia_pro - ', error)
       }
-    } catch (error) {
-      console.log('experiencia_pro - ', error)
     }
 
-    try {
-      await Formacao.destroy({
-        where: { usuarios_id: id },
-      })
-      for (let i = 0; i < body.formacaoCurso.length; i++) {
-        await Formacao.create({
-          curso: body.formacaoCurso[i],
-          instituicao: body.formacaoInstituicao[i],
-          grau: body.formacaoGrau[i],
-          inicio: body.formacaoInicio[i],
-          termino: body.formacaoTermino[i],
-          usuarios_id: id,
+    if(body.formacaoCurso){
+      try {
+        await Formacao.destroy({
+          where: { usuarios_id: id },
         })
+        for (let i = 0; i < body.formacaoCurso.length; i++) {
+          await Formacao.create({
+            curso: body.formacaoCurso[i],
+            instituicao: body.formacaoInstituicao[i],
+            grau: body.formacaoGrau[i],
+            inicio: body.formacaoInicio[i],
+            termino: body.formacaoTermino[i],
+            usuarios_id: id,
+          })
+        }
+      } catch (error) {
+        console.log('formacao - ', error)
       }
-    } catch (error) {
-      console.log('formacao - ', error)
     }
 
-    try {
-      await Usuarios_redes.destroy({
-        where: { usuarios_id: id },
-      })
-      for (let i = 0; i < body.redes.length; i++) {
-        await Usuarios_redes.create({
-          redes_id: body.redes[i],
-          link: body.redesLinks[i],
-          usuarios_id: id,
+    if(body.redes){
+      try {
+        await Usuarios_redes.destroy({
+          where: { usuarios_id: id },
         })
+        for (let i = 0; i < body.redes.length; i++) {
+          await Usuarios_redes.create({
+            redes_id: body.redes[i],
+            link: body.redesLinks[i],
+            usuarios_id: id,
+          })
+        }
+      } catch (error) {
+        console.log('usuarios_redes - ', error)
       }
-    } catch (error) {
-      console.log('usuarios_redes - ', error)
     }
 
-    try {
-      await Usuarios_linguagens.destroy({
-        where: { usuarios_id: id },
-      })
-      for (let i = 0; i < body.linguagens.length; i++) {
-        await Usuarios_linguagens.create({
-          linguagens_id: body.linguagens[i],
-          usuarios_id: id,
+    if(body.linguagens){
+      try {
+        await Usuarios_linguagens.destroy({
+          where: { usuarios_id: id },
         })
+        for (let i = 0; i < body.linguagens.length; i++) {
+          await Usuarios_linguagens.create({
+            linguagens_id: body.linguagens[i],
+            usuarios_id: id,
+          })
+        }
+      } catch (error) {
+        console.log('usuarios_linguagens - ', error)
       }
-    } catch (error) {
-      console.log('usuarios_linguagens - ', error)
     }
-
     fs.writeFileSync(path.resolve('./log', 'body.json'), JSON.stringify(body))
     res.redirect('/perfil')
   },
