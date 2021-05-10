149968e1826672b67a6a43314d6b3212c6633c57
Rafael Sakamoto <rafael.sakamoto@gmail.com>
2021-05-10 15:54:38 -0300

    api cidade/estado implementada

---
+++ public/js/localidadesApi.js
@@ -0,0 +1,49 @@
+const estadoSelect = document.getElementById('estado')
+const cidadeSelect = document.getElementById('cidade')
+
+async function getCidades(uf) {
+  const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+  const cidades = await response.json()
+  return cidades
+}
+
+async function getEstados() {
+  const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+  const estados = await response.json()
+  return estados
+}
+
+
+async function criarOptionsEstados() {
+  let ufs = await getEstados()
+  let html = "<option value=null default>Selecione um estado</option>"
+  
+  ufs.forEach(estado => {
+    html += `<option value=${estado.sigla}>${estado.nome}</option>`
+  });
+  
+  return html
+}
+
+async function criarOptionsCidades(uf) {
+  let cidades = await getCidades(uf)
+  let html = ""
+  
+  cidades.forEach(cidade => {
+    html += `<option value=${cidade.nome}>${cidade.nome}</option>`
+  });
+  
+  return html
+}
+
+async function preencherSelectEstados() {
+  estadoSelect.innerHTML = await criarOptionsEstados()
+}
+
+preencherSelectEstados()
+
+estadoSelect.addEventListener('change', async (element)=> {
+  let estado = estadoSelect.value
+  let cidadesHTML = await criarOptionsCidades(estado)
+  cidadeSelect.innerHTML = cidadesHTML
+})
